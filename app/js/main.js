d3cadbc4a9bbfa36b86be0b6d05790d849699235
Maxim <bevzmaxim@ukr.net>
2020-09-30 11:24:31 +0300

    Section Gallery done

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+
+window.addEventListener('DOMContentLoaded', function() {
+  const slider = document.querySelector('.gallery__list')
+  slide = document.querySelectorAll('.gallery__list_item');
+
+  slide.forEach((item) => {
+    item.addEventListener('click', () => {
+
+    });
+  });
+});
+
